4b9193b7b828cfb12f4e7c2f24c54d450ba8d2b6
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2024-10-17 15:32:38 +1100

    Initial Commit

---
+++ EarlSaver/EarlSaverView.h
@@ -0,0 +1,12 @@
+//
+//  EarlSaverView.h
+//  EarlSaver
+//
+//  Created by Vino Rodrigues on 17/10/24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface EarlSaverView : ScreenSaverView
+
+@end
